efbbadd1f006c28f1914c8dd8ed9dc560ec7992c
kahlil iman <kahlil.iman@gmail.com>
2020-10-13 19:58:02 +0700

    change app.listen to process.env.PORT

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-app.listen(port, () => console.log(`Test Tracking API listening at http://localhost:${port}`));
+app.listen(process.env.PORT || 3000, () => console.log(`Test Tracking API listening at http://localhost:${port}`));
