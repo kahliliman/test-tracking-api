ec01947c58deb8f43a0389c6c475b6986c938d5e
kahlil iman <kahlil.iman@gmail.com>
2020-10-13 20:00:18 +0700

    fix bug

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
+const portNum = 3000;
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-app.listen(process.env.PORT || 3000, () => console.log(`Test Tracking API listening at http://localhost:${port}`));
+app.listen(process.env.PORT || portNum, () => console.log(`Test Tracking API listening at http://localhost:${portNum}`));
