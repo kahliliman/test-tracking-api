0fa7d552a69623bd72edf3267e9cb1f2f29d41f3
kahlil iman <kahlil.iman@gmail.com>
2020-10-13 23:02:11 +0700

    create working endpoint

--- app.js
+++ app.js
@@ -3,6 +3,28 @@ const app = express();
 const HOST = '0.0.0.0';
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello world!'));
+let posts = require('./db/posts.json');
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+
+app.get('/api/v1/posts', (req, res) => {
+    res.status(200).json(posts);
+});
+
+
+app.post('/api/v1/posts', (req, res) => {
+    const { body } = req.body;
+
+    const post = { body }
+
+    posts.push(post);
+
+    res.status(201).json(post);
+});
 
 app.listen(PORT, HOST, () => console.log(`Test Tracking API listening on ${PORT}`));
